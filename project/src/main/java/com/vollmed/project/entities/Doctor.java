fa475abea24cd01c0ca8f57f5cc8e4b88b4c534c
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-05 11:00:53 -0300

    Class Doctor

---
+++ project/src/main/java/com/vollmed/project/entities/Doctor.java
@@ -0,0 +1,30 @@
+package com.vollmed.project.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "tb_doctor")
+@Entity(name = "Doctor")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Specialty specialty;
+
+    @Embedded
+    private Address address;
+
+}
