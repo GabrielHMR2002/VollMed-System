136d2343076bb806084155393099d87e1750f59f
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-05 11:09:21 -0300

    Creating record RequestAddress

---
+++ project/src/main/java/com/vollmed/project/entities/records/RequestAddress.java
@@ -0,0 +1,5 @@
+package com.vollmed.project.entities.records;
+
+public record RequestAddress(String street, String neighborhood, String postalCode, String city, String state, String complement, String number) {
+}
+
