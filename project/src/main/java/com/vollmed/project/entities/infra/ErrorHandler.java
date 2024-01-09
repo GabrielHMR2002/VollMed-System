827fcbb7d3c29890ff7320a8397d5b5af4041eb7
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-08 23:08:41 -0300

    Dependency Spring Security

--- project/src/main/java/com/vollmed/project/entities/infra/ErrorHandler.java
+++ project/src/main/java/com/vollmed/project/entities/infra/ErrorHandler.java
@@ -3,6 +3,7 @@ package com.vollmed.project.entities.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ErrorHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity ErrorHandler404(){
+    public ResponseEntity errorHandler404() {
 
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity errorHandler400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros);
     }
 
 }
