3e75fd4b6534482bbf6e9e7a94790ef7143d2863
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-09 11:56:25 -0300

    class UserService

--- project/src/main/java/com/vollmed/project/repositories/UserRepository.java
+++ project/src/main/java/com/vollmed/project/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.vollmed.project.repositories;
 
 import com.vollmed.project.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
 }
